5e78dec46fbf0ecf344689b09061628064315b2b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-11-19 03:09:51 +0700

    converted to es6 module

--- dist/validations.jsx
+++ dist/validations.jsx
@@ -1,27 +1,5 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = exports.ValidationProvider = exports.usePropValidation = exports.Context = void 0;
 // react:
-const react_1 = __importStar(require("react")); // base technology of our nodestrap components
+import { default as React, createContext, useContext, } from 'react'; // base technology of our nodestrap components
 // defaults:
 const _defaultEnableValidation = true;
 const _defaultIsValid = undefined;
@@ -29,15 +7,15 @@ const _defaultInheritValidation = true;
 /**
  * A react context for validation stuff.
  */
-exports.Context = (0, react_1.createContext)(/*defaultValue :*/ {
+export const Context = createContext(/*defaultValue :*/ {
     enableValidation: _defaultEnableValidation,
     isValid: _defaultIsValid,
 });
-exports.Context.displayName = 'Validation';
+Context.displayName = 'Validation';
 // hooks:
-const usePropValidation = (props) => {
+export const usePropValidation = (props) => {
     // contexts:
-    const valContext = (0, react_1.useContext)(exports.Context);
+    const valContext = useContext(Context);
     const inheritValidation = (props.inheritValidation ?? _defaultInheritValidation);
     const enableValidation = ((inheritValidation
         ?
@@ -65,14 +43,12 @@ const usePropValidation = (props) => {
         isValid: isValid,
     };
 };
-exports.usePropValidation = usePropValidation;
-function ValidationProvider(props) {
-    return (<exports.Context.Provider value={{
+export function ValidationProvider(props) {
+    return (<Context.Provider value={{
             enableValidation: props.enableValidation ?? _defaultEnableValidation,
             isValid: props.isValid,
         }}>
             {props.children}
-        </exports.Context.Provider>);
+        </Context.Provider>);
 }
-exports.ValidationProvider = ValidationProvider;
-exports.default = ValidationProvider;
+export { ValidationProvider as default };
